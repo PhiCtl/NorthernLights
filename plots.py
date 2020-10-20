586335ef0acd3600331ca82b908d9f4f2525769a
ch-epfl-282643 <ch-epfl-282643@noto.epfl.ch>
2020-10-20 12:34:15 +0000

    cross_validation_jets

--- plots.py
+++ plots.py
@@ -20,9 +20,26 @@ def cross_validation_visualization(param, rmse_tr, rmse_te, param_type):
 
 
 
+def cross_validation_jets(param, rmse_te_list, param_type):
+    
+    colours = ['b','r','g','m']
+    jets = np.arange(4)
+               
+    for (rmse_te, col, jet_num) in zip(rmse_te_list, colours, jets):
+        txt = 'test error' + jet_num
+        plt.semilogx(param, rmse_te, marker=".", color= col, label=txt)
+    
+    plt.xlabel(param_type)
+    plt.ylabel("rmse")
+    plt.title("cross validation")
+    plt.legend(loc=2)
+    plt.grid(True)
+    
+    plt.savefig("cross validation all jets 1 method for " + param_type)
+
+
 def plot_variance(rmse_te, param):
     fig, ax1 = plt.subplots()
     title = 'RMSE test data versus ' + param
     ax1.set_title(title)
     ax1.boxplot(rmse_te.T)
-    
