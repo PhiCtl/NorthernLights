c7be5ce00c890bfc2a4554e6f25a1712a3cdcbdd
ch-epfl-282643 <ch-epfl-282643@noto.epfl.ch>
2020-10-24 16:21:32 +0000

    final accuracy graph

--- plots.py
+++ plots.py
@@ -17,4 +17,69 @@ def cross_validation_visualization(param, rmse_tr, rmse_te, param_type):
     plt.legend(loc=2)
     plt.grid(True)
     plt.savefig("fig/cross validation for " + param_type)
+    
+def accuracy_visualization(y1,y2,y3):
+    
 
+# Define data for chart
+    x = np.linspace(1,15,15)
+    #y1 = acc_LS
+    #y2 = acc_LR
+    #y3 = acc_RegLogReg
+    
+
+# Set up figure
+    fig = plt.figure(figsize=(10,5))
+    ax = fig.add_subplot(111)
+
+# Plot Least_squares accuracy
+    i1=ax.plot(x, y1,
+            color = 'k',
+            linestyle = '-',
+            markersize = 8,
+            marker = 'o',
+            markerfacecolor='k',
+            markeredgecolor='k',
+            label  = 'Least_squares accuracy')
+
+
+
+# Plot Ridge regression 
+    i2=ax.plot(x, y2,
+            color = 'b',
+            linestyle = '-',
+            markersize = 8,
+            marker = 'o',
+            markerfacecolor='b',
+            markeredgecolor='b',
+            label  = 'Ridge regression')
+
+
+# Plot Regularized logistic regression 
+    i3=ax.plot(x, y3,
+            color = 'c',
+            linestyle = '-',
+            markersize = 8,
+            marker = 'o',
+            markerfacecolor='c',
+            markeredgecolor='c',
+            label  = 'Regularized logistic regression')
+
+# Axes
+    ax.grid(True, which='both')
+    ax.set_xlabel('Degrees')
+    ax.set_ylabel('Accuracy')
+    ax.set_xscale("linear")
+
+#Legend
+    ax.legend()
+    line_labels = ["Least squares", "Ridge regression", "Regularized Logistic regression"]
+
+    ax.legend([i1, i2, i3],              # List of the line objects
+           labels= line_labels,       # The labels for each line
+           loc="center right",        # Position of the legend
+           borderaxespad=0.1,         # Add little spacing around the legend box
+           title="Legend Title")  
+# Show plot
+    plt.show()
+    plt.savefig("fig/accuracy_plot")
